f40c04378eae5642918a4d4efb8fc451753f90f2
ivansakharov <ivan.sakharov07@gmail.com>
2016-02-26 17:51:53 -0800

    Create typing_picker.py

---
+++ typing_picker.py
@@ -0,0 +1,9 @@
+# import the magic random picker
+
+# list of things to pick from
+list=['abc','defg','rst']
+# find a way to pick something
+typing_choice=list[0]
+# a way to tell the user what to do
+
+print typing_choice
